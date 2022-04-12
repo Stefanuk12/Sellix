b901a1c854aefe90cc52be34e08d6ae58956f40a
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2022-04-12 13:52:18 +0100

    Fixed verify webhook + added express middleware

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // Dependencies
 import got, { Got } from "got"
 import * as crypto from 'crypto'
+import { NextFunction, Request, response, Response } from "express"
 
 // Exporting Models
 export { Blacklist } from "./Models/Blacklist"
@@ -25,12 +26,14 @@ export interface StandardHttpResponse {
 export class Sellix {
     // Vars
     static apiKey: string
+    static webhookSecret: string
     static apiBase: string = "https://dev.sellix.io/v1"
     static HttpClient: Got
 
     // Constructor
-    constructor(apiKey: string){
+    constructor(apiKey: string, webhookSecret: string){
         Sellix.apiKey = apiKey
+        Sellix.webhookSecret = webhookSecret
         Sellix.HttpClient = got.extend({
             prefixUrl: Sellix.apiBase,
             headers: {
@@ -40,32 +43,34 @@ export class Sellix {
         })
     }
 
+    // Verify is a webhook is legit
+    verifyWebhook(GivenSignature: string, Payload: Object){
+        // Generate Hmac
+        const PayloadString = JSON.stringify(Payload)
+        const Signature = crypto.createHmac('sha512', Sellix.webhookSecret).update(PayloadString).digest('hex')
 
-    // Verify if a webhook is valid
-    private hashEquals(answer: string, guess: string){
-        // Vars
-        const rb = crypto.pseudoRandomBytes(32)
-        const ahmac = crypto.createHmac('sha256', rb).update(answer).digest('hex')
-        const ghmac = crypto.createHmac('sha256', rb).update(guess).digest('hex')
-        const len = ahmac.length
-
-        // Loop
-        let result = 0
-        for (let i = 0; i < len; ++i) {
-            result |= (ahmac.charCodeAt(i) ^ ghmac.charCodeAt(i))
-        }
+        // Convert to buffers
+        const GivenSignatureBuffer = Buffer.from(GivenSignature)
+        const SignatureBuffer = Buffer.from(Signature)
 
         // Return
-        return result === 0
+        return crypto.timingSafeEqual(GivenSignatureBuffer, SignatureBuffer)
     }
 
-    // Verify is a webhook is legit
-    verifyWebhook(xSellixSignature: string, webhookSecret: string, requestBody: Object){
-        // Generate Hmac
-        const generatedHmac = crypto.createHmac('sha512', webhookSecret).update(JSON.stringify(requestBody)).digest('hex')
-
-        // Return
-        const isValid = this.hashEquals(generatedHmac, xSellixSignature)
-        return isValid
+    // An express middleware to check if a webhook is legit
+    verifyWebhookExpress(Request: Request, Response: Response, Next: NextFunction){
+        // Get the signature
+        let GivenSignature = Request.headers["x-sellix-signature"]
+        if (!GivenSignature){
+            return response.sendStatus(401)
+        }
+    
+        // Make sure it matches
+        if (!this.verifyWebhook(GivenSignature.toString(), Request.body)){
+            return response.sendStatus(401)
+        }
+    
+        //
+        Next()
     }
 }
